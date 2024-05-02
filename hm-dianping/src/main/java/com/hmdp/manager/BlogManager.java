10e79c6a98c116af91ff28d5a5e6d57758fd3473
Powerveil <1968103220@qq.com>
2024-05-02 16:28:24 +0800

    查看博客

---
+++ hm-dianping/src/main/java/com/hmdp/manager/BlogManager.java
@@ -0,0 +1,91 @@
+package com.hmdp.manager;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.constant.SystemConstants;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
+import com.hmdp.service.IBlogService;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @ClassName BlogManager
+ * @Description TODO(一句话描述该类的功能)
+ * @Author Powerveil
+ * @Date 2024/5/2 16:11
+ * @Version 1.0
+ */
+@Component
+@Slf4j
+public class BlogManager {
+
+    @Autowired
+    private IBlogService blogService;
+
+    @Autowired
+    private IUserService userService;
+
+    public Result saveBlog(Blog blog) {
+        // 获取登录用户
+        UserDTO user = UserHolder.getUser();
+        blog.setUserId(user.getId());
+        // 保存探店博文
+        blogService.save(blog);
+        // 返回id
+        return Result.ok(blog.getId());
+    }
+
+    public Result likeBlog(Long id) {
+        // 修改点赞数量
+        blogService.update()
+                .setSql("liked = liked + 1").eq("id", id).update();
+        return Result.ok();
+    }
+
+    public Result queryMyBlog(Integer current) {
+        // 获取登录用户
+        UserDTO user = UserHolder.getUser();
+        // 根据用户查询
+        Page<Blog> page = blogService.query()
+                .eq("user_id", user.getId()).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        return Result.ok(records);
+    }
+
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = blogService.query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(this::queryBlogUser);
+        return Result.ok(records);
+    }
+
+    private void queryBlogUser(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
+
+    public Result queryBlogById(Long id) {
+        // 1.查询Blog
+        Blog blog = blogService.getById(id);
+        if (Objects.isNull(blog)) {
+            return Result.fail("博文不存在");
+        }
+        this.queryBlogUser(blog);
+        return Result.ok(blog);
+    }
+}
