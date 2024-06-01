804b2a8ca9842823a4b6a19d7350cbc1b9176568
Powerveil <1968103220@qq.com>
2024-06-01 09:55:06 +0800

    测试key过期监听

--- hm-dianping/src/main/java/com/hmdp/listener/RedisKeyExpireListener.java
+++ hm-dianping/src/main/java/com/hmdp/listener/RedisKeyExpireListener.java
@@ -1,9 +1,16 @@
 package com.hmdp.listener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @ClassName RedisKeyExpireListener
@@ -14,6 +21,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class RedisKeyExpireListener extends KeyExpirationEventMessageListener {
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     public RedisKeyExpireListener(RedisMessageListenerContainer listenerContainer) {
         super(listenerContainer);
@@ -28,5 +37,16 @@ public class RedisKeyExpireListener extends KeyExpirationEventMessageListener {
         String expireKey = message.toString();
         System.out.println("过期的key：" + expireKey);
         // 根据key,执行自己需要实现的功能。
+//        Map<String, Integer> map = new HashMap<>();
+//        if (expireKey.contains("test:order:")) {
+//            BoundHashOperations<String, String, Integer> hashOperations = redisTemplate.boundHashOps(expireKey);
+//            // 这是key已经被删除，无法获取数据
+//            Set<String> keys = hashOperations.keys();
+//            for (String key : keys) {
+//                Integer value = hashOperations.get(key);
+//                map.put(key, value);
+//            }
+//        }
+//        System.out.println(map.toString());
     }
 }
